0fd40186250d6d124cec47ad0199481da3d52cd9
haeeun <109775015+heaeunin@users.noreply.github.com>
2023-01-06 20:53:44 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,78 @@
+//랜덤번호 지정
+//유저가 번호를 입력한다 그리고 go 라는 버튼을 누름
+//만약에 유저가 랜덤번호를 맞추면, 맞췄습니다!
+//랜덤번호가 < 유저번호 Down!!!
+//랜덤번호가 > 유저번호 Up!!!
+//Rest 버튼을 누르면 게임이 리셋된다.
+//5번의 기회를 다쓰면 게임이 끝난다(더이상 추측 불가, 버튼이 disable)
+//유저가 1~100 범위 밖에 숫자를 입력하면 알려준다. 기회를 깍지 않는다.
+//유저가 이미 입력한 숫자를 또 입력하면, 알려준다, 기회를 깍지 않는다.
+
+let computerNum = 0;
+let playButton = document.getElementById('play-button');
+let userInput = document.getElementById('user-input');
+let resultArea = document.getElementById('result-area');
+let resetButton = document.getElementById('reset-button');
+let chanceArea = document.getElementById('chance-area');
+let chances = 10;
+let gameOver = false;
+let history =[];
+
+
+playButton.addEventListener('click', play);
+resetButton.addEventListener('click', reset);
+
+//랜덤번호 지정
+function pickRandomNun(){
+  computerNum = Math.floor(Math.random() * 100) +1;
+  //랜덤한 숫자를 뽑을 수 있게 도와주는 함수
+  console.log('정답', computerNum);
+}
+
+function play(){
+  let userValue = userInput.value;
+
+  if(userValue < 1 || userValue > 1000){
+    resultArea.textContent= "1과 1000사이 숫자를 입력해주세요"
+    return;
+  }
+
+  if(history.includes(userValue)){
+    resultArea.textContent = "이미 입력한 숫자입니다 다른 숫자를 입력해주세요"
+    return;
+  }
+  
+  chances --; //1씩 줄어듬
+  chanceArea.textContent=`남은 기회 : ${chances}번`;
+
+  if(userValue < computerNum){
+    resultArea.textContent = 'Up!'
+  }else if(userValue > computerNum ){
+    resultArea.textContent = 'Down!'
+  }else{
+    resultArea.textContent = '정답입니다!'
+    gameOver=true
+  }
+
+  history.push(userValue)
+
+  if(chances < 1){
+    gameOver = true;
+  }
+  if(gameOver == true){
+    playButton.disabled = true;
+  }
+}
+
+function reset(){
+  //user input창이 깨끗하게 정리되고
+  userInput.value = "";
+  //새로분 번호가 생성되고
+  pickRandomNun();
+
+  resultArea.textContent = "결과값이 여기 나옵니다!";
+}
+
+pickRandomNun();
+
+
